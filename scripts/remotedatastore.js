b2165a638ac328c44e409e7ef09947cfdb4ab754
Daniel Bravo <bravoappdev@gmail.com>
2018-03-12 18:04:38 -0700

    Modify app to use a local remote datastore.

--- scripts/remotedatastore.js
+++ scripts/remotedatastore.js
@@ -4,6 +4,7 @@
   var $ = window.jQuery;
 
   function RemoteDataStore(url) {
+    this.data = {};
     if (!url) {
       throw new Error("No remote URL supplied.");
     }
@@ -13,8 +14,9 @@
 
   RemoteDataStore.prototype.add = function (key, val) {
     $.post(this.serverUrl, val, function (serverResponse) {
+      this.data[key] = serverResponse.id;
       console.log(serverResponse);
-    });
+    }.bind(this));
   };
 
   RemoteDataStore.prototype.getAll = function (cb) {
@@ -25,14 +27,14 @@
   };
 
   RemoteDataStore.prototype.get = function (key, cb) {
-    $.get(this.serverUrl + "/" + key, function (serverResponse) {
+    $.get(this.serverUrl + "/" + this.data[key], function (serverResponse) {
       console.log(serverResponse);
       cb(serverResponse);
     });
   };
 
   RemoteDataStore.prototype.remove = function (key) {
-    $.ajax(this.serverUrl + "/" + key, {
+    $.ajax(this.serverUrl + "/" + this.data[key], {
       type: "DELETE"
     });
   };
